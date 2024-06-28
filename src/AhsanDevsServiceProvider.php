7d7f532b539fe232db3284a66cc0b972403224fb
Ahsan Abrar <iahsanabrar@gmail.com>
2024-06-28 19:08:09 +0500

    first commit

---
+++ src/AhsanDevsServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace AhsanDevs;
+
+use Illuminate\Support\ServiceProvider;
+
+class AhsanDevsServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any package services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
