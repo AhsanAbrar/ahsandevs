b9114af7171af56f4d70a8e59b647d77caf64e47
Ahsan Abrar <iahsanabrar@gmail.com>
2024-07-23 14:23:14 +0500

    base authorization

--- src/Console/BaseCommand.php
+++ src/Console/BaseCommand.php
@@ -16,6 +16,7 @@ class BaseCommand extends Command implements PromptsForMissingInput
      * @var string
      */
     protected $signature = 'ahsandevs:base
+                            {--authorization : Add authorization support}
                             {--options : Add options migrations}';
 
     /**
@@ -34,6 +35,10 @@ class BaseCommand extends Command implements PromptsForMissingInput
             $this->addOptions();
         }
 
+        if ($this->option('authorization')) {
+            $this->addAuthorization();
+        }
+
         $this->info('Laravel base updated successfully.');
     }
 
@@ -47,6 +52,25 @@ class BaseCommand extends Command implements PromptsForMissingInput
         $destination = base_path('database/migrations/0002_02_02_000001_create_options_table.php');
 
         if (!$filesystem->exists($destination)) {
+            $filesystem->copy($source, $destination);
+        }
+    }
+
+    /**
+     * Add Authorization.
+     */
+    protected function addAuthorization(): void
+    {
+        $filesystem = new Filesystem;
+        $source = __DIR__ . '/../../base/database/migrations/0002_02_02_000002_create_authorizations_table.php';
+        $destination = base_path('database/migrations/0002_02_02_000002_create_authorizations_table.php');
+
+        if (!$filesystem->exists($destination)) {
+            $filesystem->copy($source, $destination);
+
+            $source = __DIR__ . '/../../base/app/Models/User.php';
+            $destination = base_path('app/Models/User.php');
+
             $filesystem->copy($source, $destination);
         }
     }
