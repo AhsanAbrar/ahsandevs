3bdce76ce75f0e62ddd1506c80b4da5e6f30cfd0
Ahsan Abrar <iahsanabrar@gmail.com>
2024-08-06 12:44:26 +0500

    root is done

---
+++ stubs/vue-thetheme/tailwind.config.js
@@ -0,0 +1,18 @@
+import defaultTheme from 'tailwindcss/defaultTheme'
+import forms from '@tailwindcss/forms'
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: 'class',
+  content: ['./resources/**/*.{js,ts,tsx,vue,blade.php}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [
+    forms,
+  ],
+}
