ca195a016c091c748e664b146bdd84590518f87e
Ahsan Abrar <iahsanabrar@gmail.com>
2024-08-07 16:07:05 +0500

    Update AhsanDevsServiceProvider.php

--- src/AhsanDevsServiceProvider.php
+++ src/AhsanDevsServiceProvider.php
@@ -40,7 +40,7 @@ class AhsanDevsServiceProvider extends ServiceProvider
             Console\InstallCommand::class,
             // Console\PackageCommand::class,
             // Console\BaseCommand::class,
-            // Console\ResourceCommand::class,
+            Console\ResourceCommand::class,
             Console\ControllerCommand::class,
             Console\FilterCommand::class,
             Console\FiltersCommand::class,
