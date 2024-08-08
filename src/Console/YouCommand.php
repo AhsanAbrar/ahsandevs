9ed3ff816340ada224d37d48c7e19ff8afda25b6
Ahsan Abrar <iahsanabrar@gmail.com>
2024-08-08 18:46:21 +0500

    Update YouCommand.php

--- src/Console/YouCommand.php
+++ src/Console/YouCommand.php
@@ -12,13 +12,24 @@ class YouCommand extends Command implements PromptsForMissingInput
      *
      * @var string
      */
-    protected $signature = 'ahsandevs:you {name : The request name} {package : The span package dir name}';
+    protected $signature = 'ahsandevs:you {name : The request name} {package? : The span package dir name}';
 
     /**
      * Execute the console command.
      */
     public function handle()
     {
-        $this->info('Name: ' . $this->argument('name') . ' | Package: ' . $this->argument('package'));
+        $name = $this->argument('name');
+        $package = $this->argument('package');
+
+        if (!$package) {
+            // Define the options
+            $options = ['First', 'Second', 'Third'];
+
+            // Prompt the user to select an option
+            $package = $this->choice('Please select a package:', $options);
+        }
+
+        $this->info('Name: ' . $name . ' | Package: ' . $package);
     }
 }
