b21d6e5d9e0eda095f7637cecf45c3dee1c970dc
Ahsan Abrar <iahsanabrar@gmail.com>
2024-08-08 22:36:48 +0500

    Update Command.php

--- src/Console/Command.php
+++ src/Console/Command.php
@@ -6,6 +6,8 @@ use AhsanDevs\Console\Concerns\CommandHelpers;
 use Illuminate\Console\Command as BaseCommand;
 use Illuminate\Contracts\Console\PromptsForMissingInput;
 use Illuminate\Filesystem\Filesystem;
+use Illuminate\Support\Facades\File;
+use function Laravel\Prompts\select;
 
 class Command extends BaseCommand implements PromptsForMissingInput
 {
@@ -17,5 +19,32 @@ class Command extends BaseCommand implements PromptsForMissingInput
     public function __construct(protected Filesystem $filesystem)
     {
         parent::__construct();
+    }
+
+    /**
+     * Prompt for missing input arguments using the returned questions.
+     */
+    protected function promptForMissingArgumentsUsing(): array
+    {
+        return [
+            'package' => fn () => $this->choice('Please select a package or write package name', $this->packageOptions()),
+            // 'package' => fn () => select('Please select a package or write package name', $this->packageOptions()),
+        ];
+    }
+
+    /**
+     * Get the package options.
+     */
+    protected function packageOptions(): array
+    {
+        $directories = File::directories(base_path('packages'));
+
+        $options = [];
+
+        foreach ($directories as $directory) {
+            $options[] = basename($directory);
+        }
+
+        return $options;
     }
 }
