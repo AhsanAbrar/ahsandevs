b5fb75537a3f876c42d182fd2d8b3e13203b5b11
Ahsan Abrar <iahsanabrar@gmail.com>
2024-08-08 18:24:48 +0500

    Update sidebar-nav.ts

--- packages/vue-thetheme/resources/js/composables/sidebar-nav.ts
+++ packages/vue-thetheme/resources/js/composables/sidebar-nav.ts
@@ -2,5 +2,5 @@ import { HomeIcon, UserIcon } from '@heroicons/vue/24/outline'
 
 export const useSidebarNav: SidebarNav[] = [
   { label: 'Dashboard', uri: '/', icon: HomeIcon },
-  { label: 'Users', uri: '/users', icon: UserIcon }
+  { label: 'Users', uri: '/users', icon: UserIcon },
 ]
