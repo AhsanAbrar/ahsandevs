722c0f27a0f53b13bc60d8fd6594a8a921e06470
Ahsan Abrar <iahsanabrar@gmail.com>
2024-08-09 15:23:46 +0500

    Update ControllerCommand.php

--- src/Console/ControllerCommand.php
+++ src/Console/ControllerCommand.php
@@ -4,6 +4,7 @@ namespace AhsanDevs\Console;
 
 use AhsanDevs\Console\Concerns\StubHelpers;
 use AhsanDevs\Console\Concerns\StubReplaceHelpers;
+use Illuminate\Support\Str;
 
 class ControllerCommand extends Command
 {
@@ -15,6 +16,7 @@ class ControllerCommand extends Command
      * @var string
      */
     protected $signature = 'ahsandevs:controller {name : The controller name} {package : The span package dir name}
+                            {--s|save : Generate a create and store methods controller class}
                             {--i|invokable : Generate a single method, invokable controller class}';
 
 
@@ -26,12 +28,22 @@ class ControllerCommand extends Command
     protected $description = 'Generate controller class';
 
     /**
+     * Get the name argument.
+     */
+    protected function getNameArgument(): string
+    {
+        return Str::before($this->argument('name'), 'Controller');
+    }
+
+    /**
      * Execute the console command.
      */
     public function handle()
     {
         if ($this->option('invokable')) {
             $this->generateStub('ControllerInvokable.stub', 'src/Http/Controllers/Api/'.$this->pascalName().'.php');
+        } elseif ($this->option('save')) {
+            $this->generateStub('ControllerSave.stub', 'src/Http/Controllers/Api/'.$this->pascalName().'.php');
         } else {
             $this->generateStub('Controller.stub', 'src/Http/Controllers/Api/'.$this->pascalName().'.php');
         }
