486b2aa9684b3369e7f4deb8eaee7383f4ec9f73
Ahsan Abrar <iahsanabrar@gmail.com>
2025-03-11 17:16:15 +0500

    Update main.ts

--- packages/vue-thetheme/resources/js/main.ts
+++ packages/vue-thetheme/resources/js/main.ts
@@ -3,7 +3,7 @@ import { router } from './router'
 import { createPinia } from 'pinia'
 import { createAhsandevs } from 'ahsandevs'
 import './tailwind.css'
-import 'thetheme/dist/style.css'
+import 'thetheme/dist/thetheme.css'
 
 app
   .use(createAhsandevs)
